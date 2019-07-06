c2ad1ac2f27cc30da8f98c92f79e9853b2ed39ad
S. M. Shabriar Hossain <shabriar@encodersbd.com>
2019-07-06 12:35:50 +0600

    all crud with sock in out in Add

---
+++ Stationary Management/Stationary Management/Entity/Requisition.cs
@@ -0,0 +1,26 @@
+﻿using SCHM.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Stationary_Management.Entity
+{
+    public class Requisition: AuditableEntity
+    {
+        [Display(Name = "Product")]
+        public int? ProductsId { get; set; }
+        [ForeignKey("ProductsId")]
+        public virtual Products Products { get; set; }
+
+        [Display(Name = "Requisition Quantity")]
+        public double RequisitionQuantity { get; set; }
+        [Display(Name = "Requisition No")]
+        public double RequisitionNo { get; set; }
+
+        [Display(Name = "Status")]
+        public  bool Status { get; set; }
+    }
+}
---
+++ Stationary Management/Stationary Management/Repository/RequisitionUnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿
+using Stationary_Management.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SCHM.Repo
+{
+    public class RequisitionUnitOfWork : IDisposable
+    {
+        private RequisitionRepository _requisitionRepository;
+        private SCHMDbContext _context;
+        public RequisitionUnitOfWork(SCHMDbContext context)
+        {
+            _context = context;
+            _requisitionRepository = new RequisitionRepository(_context);
+        }
+        public RequisitionRepository RequisitionRepository => _requisitionRepository;
+
+
+        public void Save(string currentUserId)
+        {
+            _context.SaveChanges();
+        }
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
