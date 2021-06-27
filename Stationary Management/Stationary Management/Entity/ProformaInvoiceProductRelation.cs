754cc5f3b3affcc4d7463851cf5f1294d2e82aa3
rockersabbir <rockersabbier@gmail.com>
2021-06-27 23:46:25 +0600

    PI initial

---
+++ Stationary Management/Stationary Management/Entity/ProformaInvoiceProductRelation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using SCHM.Entities;
+
+namespace Stationary_Management.Entity
+{
+    [Table("ProformaInvoiceProductRelation")]
+    public class ProformaInvoiceProductRelation : AuditableEntity //Entity
+    {
+
+        public int PfiId { get; set; }
+        [ForeignKey("PfiId")]
+        public virtual ProformaInvoice ProformaInvoice { get; set; }
+
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public virtual Products Product { get; set; }
+
+        //public int? AgentId { get; set; }
+        //[ForeignKey("AgentId")]
+        //public virtual GeneralConfig Agent { get; set; }
+
+
+        [Display(Name = "Unit Price")]
+        public double UnitPrice { get; set; }
+
+        [Display(Name = "Quantity")]
+        public double Quantity { get; set; }
+
+        [Display(Name = "Total Price")]
+        public double? TotalPrice { get; set; }
+
+        [Display(Name = "Shipment Mode")]
+        public string ShipmentMode { get; set; }
+        [Display(Name = "Remarks")]
+        public string Remarks { get; set; }
+
+        //public virtual ICollection<CommercialInvoiceProduct> CommercialInvoiceProductCollection { get; set; }
+    }
+}
---
+++ Stationary Management/Stationary Management/Repository/ProformaInvoiceUnitOfWork.cs
@@ -0,0 +1,47 @@
+﻿using SCHM.Repo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Stationary_Management.Repository
+{
+    public class ProformaInvoiceUnitOfWork : IDisposable
+    {
+        public SCHMDbContext _context;
+        public ProformaInvoiceRepository _ProformaInvoiceRepository;
+        public ProformaInvoiceProductRelationRepository _proformaInvoiceProductRelationRepository;
+        public ProformaInvoiceUnitOfWork(SCHMDbContext context)
+        {
+            _context = context;
+            _ProformaInvoiceRepository = new ProformaInvoiceRepository(_context);
+            _proformaInvoiceProductRelationRepository = new ProformaInvoiceProductRelationRepository(_context);
+        }
+        public ProformaInvoiceRepository ProformaInvoiceRepository
+        {
+            get
+            {
+                return _ProformaInvoiceRepository;
+            }
+        }
+        public ProformaInvoiceProductRelationRepository ProformaInvoiceProductRelationRepository
+        {
+            get
+            {
+                return _proformaInvoiceProductRelationRepository;
+            }
+        }
+        public void Save(string loggedInUserId)
+        {
+            _context.SaveChanges();
+        }
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
